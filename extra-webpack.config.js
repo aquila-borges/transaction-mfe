d0cc21ab4769116bc68426437fb26e384c257915
Áquila Borges Moreira <aquila.brgs@gmail.com>
2025-08-11 05:05:28 -0300

    Remoção de importação desnecessária de 'zone.js' no arquivo main.ts

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -3,9 +3,5 @@ const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').defaul
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
 
-  // Externalize zone.js para evitar carregamento duplicado
-  singleSpaWebpackConfig.externals = singleSpaWebpackConfig.externals || {};
-  singleSpaWebpackConfig.externals['zone.js'] = 'Zone';
-
   return singleSpaWebpackConfig;
 };
